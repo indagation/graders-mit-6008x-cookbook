045bc259aced928ac32f508db3cbf301a0bc2d46
Kyle Boots <kboots@mit.edu>
2016-10-14 11:48:51 -0400

    Add Git

--- graders/recipes/deploy.rb
+++ graders/recipes/deploy.rb
@@ -1,3 +1,19 @@
+bash "Remove All Cronjobs" do  
+  user 'root'
+  code 'crontab -u root -r'
+  only_if 'crontab -u root -l'
+end
+
+cron 'Update Grader Repo' do
+  minute '15'
+  user 'root'
+  mailto 'indagation@gmail.com'
+  command %W{
+    cd /home/ubuntu/graders-mit-6008x
+    git pull
+  }.join(' ')
+end
+
 execute 'Run Grader in Background' do
   cwd '/home/ubuntu/graders-mit-6008x'
   command 'nohup sudo python -m xqueue_watcher -d conf.d >> ../grader.out 2>&1&'
